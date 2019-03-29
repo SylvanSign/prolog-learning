7de043498cdf461d62a00a9e92dc4d117811daa4
sylvansign <jake.d.bray@gmail.com>
2019-03-28 22:24:38 -0400

    formatted

--- bratko/04 - Programming Examples/trip_planner.pl
+++ bratko/04 - Programming Examples/trip_planner.pl
@@ -12,12 +12,12 @@ timetable([ riva at 14:05, limone  at 14:40, malcesine at 15:00                 
 timetable([ riva at 15:00, limone  at 15:36, malcesine at 15:57, campione  at 16:13 ]).
 timetable([ riva at 16:20, torbole at 16:35, limone    at 17:05, malcesine at 17:25 ]).
 
-timetable([ malcesine at 9:00, limone at 9:20, tobole at 9:50, riva at 10:05 ]).
-timetable([ malcesine at 10:25, limone at 10:50, tobole at 11:20, riva at 11:35 ]).
-timetable([ malcesine at 11:25, limone at 11:45, riva at 12:20 ]).
-timetable([ campione at 12:55, malcesine at 13:12, limone at 13:34, riva at 14:10 ]).
-timetable([ malcesine at 13:45, limone at 13:59, riva at 14:20 ]).
-timetable([ malcesine at 15:05, limone at 15:25, riva at 16:00 ]).
-timetable([ malcesine at 16:30, limone at 16:50, tobole at 17:20, riva at 17:35 ]).
-timetable([ malcesine at 18:15, limone at 18:35, tobole at 19:05, riva at 19:20 ]).
-timetable([ malcesine at 19:15, limone at 19:35, tobole at 20:05, riva at 20:20 ]).
+timetable([ malcesine at 9:00,  limone    at 9:20,  tobole at 9:50,  riva at 10:05 ]).
+timetable([ malcesine at 10:25, limone    at 10:50, tobole at 11:20, riva at 11:35 ]).
+timetable([ malcesine at 11:25, limone    at 11:45,                  riva at 12:20 ]).
+timetable([ campione  at 12:55, malcesine at 13:12, limone at 13:34, riva at 14:10 ]).
+timetable([ malcesine at 13:45, limone    at 13:59,                  riva at 14:20 ]).
+timetable([ malcesine at 15:05, limone    at 15:25,                  riva at 16:00 ]).
+timetable([ malcesine at 16:30, limone    at 16:50, tobole at 17:20, riva at 17:35 ]).
+timetable([ malcesine at 18:15, limone    at 18:35, tobole at 19:05, riva at 19:20 ]).
+timetable([ malcesine at 19:15, limone    at 19:35, tobole at 20:05, riva at 20:20 ]).
