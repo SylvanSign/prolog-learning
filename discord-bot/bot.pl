705a59a2035fe1f1dcd6a1899ffc3bae6160c260
sylvansign <jake.d.bray@gmail.com>
2019-06-16 02:18:52 -0400

    making progress!

--- discord-bot/bot.pl
+++ discord-bot/bot.pl
@@ -1,21 +1,30 @@
-:- dynamic alive/0.
+:- (dynamic alive/0).
 :- use_module(library(http/websocket)).
 :- use_module(library(http/json)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/http_json)).
 
-connect :-
-  http_open_websocket('wss://gateway.discord.gg/?v=6&encoding=json', WS, []),
-  ws_receive(WS, Reply),
-  json_read(WS, Term).
+
+gateway_url(URL) :-
+    http_get('https://discordapp.com/api/gateway',
+             _{url:URL},
+             [json_object(dict)]).
+
+connect(Data) :-
+    gateway_url(GatewayURL),
+    string_concat(GatewayURL, "/?v=6&encoding=json", WebsocketURL),
+    http_open_websocket(WebsocketURL, WS, []),
+    ws_receive(WS, Data, [format(json)]).
 
 heartbeat :-
-  assert(alive),
-  thread_create(pulse, _).
+    assert(alive),
+    thread_create(pulse, _).
 
 kill :-
-  retract(alive).
+    retract(alive).
 
 pulse :-
-  alive,
-  writeln(ping),
-  sleep(10),
-  pulse.
+    alive,
+    writeln(ping),
+    sleep(10),
+    pulse.
