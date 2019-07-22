f2d2714202198bae59a03b23841e48b08000ac2a
Jake Bray <jake.d.bray@gmail.com>
2019-07-22 19:53:22 -0400

    lots of features

--- discord-bot/adama.pl
+++ discord-bot/adama.pl
@@ -3,7 +3,18 @@
 start :-
     jump(reply_to_message).
 
-adama --> [so,say,we,all], ([!] ; []).
+so_say_we_all --> [so,say,we,all], tail_punctuation.
+
+starbuck_exchange --> whatya, [hear], target, tail_punctuation.
+whatya --> [whatya].
+whatya --> [whatdya].
+whatya --> [what,do], ([ya] ; [you]).
+target --> [] ; [starbuck] ; [adama] ; [commander] ; [admiral].
+
+uncertainty --> [idk].
+uncertainty --> [i], ([don,'\'',t] ; [dunno]).
+
+tail_punctuation --> [] ; [!] ; [?].
 
 % This is the heart of the bot. ie. where the NLP happens
 reply_to_message(Data, Reply) :-
@@ -11,8 +22,34 @@ reply_to_message(Data, Reply) :-
     Content = Data.content,
     tokenize_as_atoms(Content, Words),
     writeln(Words),
-    phrase(adama, Words),
+    reply(Data, Words, Reply).
+
+reply(_Data, Words, Reply) :-
+    phrase(so_say_we_all, Words),
     Reply = "SO SAY WE ALL!".
+reply(_Data, Words, Reply) :-
+    phrase(starbuck_exchange, Starbuck),
+    prefix(Starbuck, Words),
+    Reply = "Nothing but the rain.".
+reply(_Data, Words, Reply) :-
+    phrase(uncertainty, UncertainPhrase),
+    sublist(UncertainPhrase, Words),
+    Reply = "Sometimes you gotta roll the hard six.".
+reply(_Data, Words, Reply) :-
+    bad(Bad),
+    member(Bad, Words),
+    Reply = "PULL YOURSELF TOGETHER!".
+
+bad(sorry).
+bad(inted).
+bad(inting).
+bad(woops).
+bad(anime).
+bad(yikes).
+
+sublist(S,L) :-
+    append(_,L2,L),
+    append(S,_,L2).
 
 % This prevents bot from ruining other people's experience ;)
 should_skip_reply(Data) :-
